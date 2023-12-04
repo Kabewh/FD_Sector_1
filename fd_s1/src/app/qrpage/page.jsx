eb3825a49eb5dafbd388b00cbf4ce2da7b9a05f0
Daniele Viola <daniele.viola02@e-uvt.ro>
2023-12-04 19:34:13 +0200

    restyled qrpage

--- fd_s1/src/app/qrpage/page.jsx
+++ fd_s1/src/app/qrpage/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import AutoplayVideo from "./Video";
 import Form from "./intrebari/Form";
 
@@ -8,10 +7,23 @@ export default function QrPage() {
 
   return (
     <>
-      <section>
-        <div className="h-screen bg-primary">
-          <div className="h-1/3"></div>
-          <AutoplayVideo videoSource={video} />
+      <section className="flex flex-col justify-center items-center bg-primary h-screen">
+        <img
+          src="/logoOrizontal.jpeg"
+          className="-mt-24 lg:w-2/4 xl:-mt-56 2xl:-mt-72"
+        />
+        <div className="sm:w-1/2 xl:-mt-24 2xl:-mt-56">
+          <AutoplayVideo videoSource={video} className="shadow-xl " />
+        </div>
+        <div className="text-white mt-12 mx-4">
+          <p className="font-bold">PARTIDUL POLITIC</p>
+          <h1 className="font-black text-4xl">Autentic de dreapta</h1>
+          <h3 className="italic font-medium">
+            Adevărații liberali sunt alături de Ludovic Orban și Forța Dreptei,
+            oameni care își respectă cuvântul dat în alegeri și care sunt loiali
+            doar cetățenilor.
+          </h3>
+          <img src="/prinNoiInsine.png" className="w-48 mt-2" />
         </div>
       </section>
     </>
