435b6ddcd818b851d4b2fc563e31c1eea3c5e7e8
Daniele Viola <daniele.viola02@e-uvt.ro>
2023-12-08 21:09:00 +0200

    adjusted video

--- fd_s1/src/app/qrpage/page.jsx
+++ fd_s1/src/app/qrpage/page.jsx
@@ -12,7 +12,7 @@ export default function QrPage() {
       <section className="flex flex-col h-full md:h-none md:h-auto md:justify-start bg-primary md:bg-transparent md:mt-12 items-center md:w-5/6 md:mx-auto md:pb-48">
         <img
           src="/logoOrizontal.jpeg"
-          className="block w-2/5 pt-2 pb-6 md:hidden lg:w-2/4 xl:-mt-56 2xl:-mt-72"
+          className="block w-2/5 py-2 md:hidden lg:w-2/4 xl:-mt-56 2xl:-mt-72"
         />
         <img
           src="/greenonwhitelogo.jpg"
@@ -21,8 +21,8 @@ export default function QrPage() {
         <div className="">
           <AutoplayVideo videoSource={video} className="shadow-xl" />
         </div>
-        <div className="md:hidden h-2/4 text-white md:mt-12 mt-4 mx-4 flex flex-col items-center justify-center text-center">
-          <p className="font-bold mb-1 font-gotham">PARTIDUL POLITIC</p>
+        <div className="md:hidden h-2/4 text-white md:mt-12 mx-2 flex flex-col items-center text-center">
+          <p className="font-bold mb-1 font-gotham mt-6">PARTIDUL POLITIC</p>
           <h1 className="text-3xl mb-1 font-gotham">Autentic de dreapta</h1>
           <h3 className="italic font-medium">
             Adevărații liberali sunt alături <br></br> de Ludovic Orban și Forța
