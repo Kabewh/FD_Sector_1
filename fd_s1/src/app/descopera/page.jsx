0717a0f838dd1c6026d5d7e21c358e3edb6bb2fe
Daniele Viola <daniele.viola02@e-uvt.ro>
2023-12-11 21:09:04 +0200

    deleted broken import

--- fd_s1/src/app/descopera/page.jsx
+++ fd_s1/src/app/descopera/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import AutoplayVideo from "./Video";
 import Navbar from "../components/Navbar";
-import Form from "./intrebari/Form";
 import { redirect } from "next/navigation";
 
 export default function QrPage() {
